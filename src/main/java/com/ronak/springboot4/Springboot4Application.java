3ddaeefd9850c3b3210488fc0d62dd273bed1f89
ronak <chaudharyronak291@gmail.com>
2026-07-01 10:04:50 +0530

    fix: format main method for improved readability

--- src/main/java/com/ronak/springboot4/Springboot4Application.java
+++ src/main/java/com/ronak/springboot4/Springboot4Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot4Application {
 
-	static void main(String[] args){SpringApplication.run(Springboot4Application.class, args);
+	static void main(String[] args) {
+		SpringApplication.run(Springboot4Application.class, args);
 	}
 
 }
